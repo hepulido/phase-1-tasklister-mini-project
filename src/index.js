eefa0db617605daa554a4d95a5ec83069df57528
hector <hpulido1021@gmail.com>
2022-04-29 18:22:26 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    createElement(e.target['new-task-description'].value)
+  })
+
 });
+
+function createElement(element){
+  let p = document.createElement("p")
+  let button = document.createElement("button")
+  button.addEventListener("click", handleDelete)
+  button.textContent = "x"
+  p.textContent = `${element} `
+  p.appendChild(button)
+  console.log(p)
+  document.querySelector("#tasks").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
